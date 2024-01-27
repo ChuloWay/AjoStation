b3ec4c3f14dfcfc2093ad33d884929da6e00bca8
ChuloWay <okoyeebuka64@gmail.com>
2024-01-27 21:18:47 +0100

    fix: register user input validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,26 +1,39 @@
-import { IsAlpha, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty({ message: 'First Name is required' })
-  @IsAlpha()
   @IsString()
+  @Matches(/^[a-zA-Z]+$/, {
+    message: 'First Name can only contain letters',
+  })
   firstName: string;
 
   @IsNotEmpty({ message: 'Last Name is required' })
-  @IsAlpha()
   @IsString()
+  @Matches(/^[a-zA-Z]+$/, {
+    message: 'Last Name can only contain letters',
+  })
   lastName: string;
 
   @IsNotEmpty()
   @IsEmail()
+  @Matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, {
+    message: 'Invalid email format',
+  })
   @Transform((params) => params.value.toLowerCase())
   email: string;
 
   @IsNotEmpty({ message: 'Phone Number is required' })
+  @Matches(/^(?:\+234|234|0)(?:\d{10})$/, {
+    message: 'Invalid phone number format',
+  })
   phoneNumber: string;
 
   @IsNotEmpty({ message: 'Password is required' })
   @IsString()
+  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,30}$/, {
+    message: 'Invalid password format',
+  })
   password: string;
 }
