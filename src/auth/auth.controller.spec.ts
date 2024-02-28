f847271673cfeacfb5e692516e1472d6410d8df1
ChuloWay <okoyeebuka64@gmail.com>
2024-02-28 14:20:12 +0100

    feat: unit tests for all services

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { UtilityService } from 'src/util/utilityService';
+import { JwtPayloadService } from './strategy/jwt-payload';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+const mockCacheManager = {
+  add: jest.fn().mockResolvedValue({}),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +18,18 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UserService,
+        JwtPayloadService,
+        UtilityService,
+        PrismaService,
+        JwtService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
+import { JwtPayloadService } from './strategy/jwt-payload';
+import { UtilityService } from 'src/util/utilityService';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+
+const mockCacheManager = {
+  add: jest.fn().mockResolvedValue({}),
+};
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UserService,
+        JwtPayloadService,
+        UtilityService,
+        PrismaService,
+        JwtService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
